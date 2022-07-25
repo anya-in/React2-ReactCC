17d35533546dc0da3bd7699fc9af05c60b6f31d1
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-07-25 13:57:01 +0530

    UI,Delete,Toggle

--- task_tracker/src/App.js
+++ task_tracker/src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Business Appointment',
+        day: ' August 2nd at 2:30 p.m.',
+        reminder: 'true'
+      },
+      {
+        id: 2,
+        text: 'Meeting',
+        day: ' August 3rd at 8:30 a.m.',
+        reminder: 'true'
+      },
+      {
+        id: 3,
+        text: 'Shopping',
+        day: ' August 5th at 12:30 p.m.',
+        reminder: 'false'
+      }
+    ]
+  );
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}
+        onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
